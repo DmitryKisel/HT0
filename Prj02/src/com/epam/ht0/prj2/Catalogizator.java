d0db176ae9e35fd879fde9490eb0801f15b5874c
Dzmitry Kissel <33577204+DmitryKissel@users.noreply.github.com>
2018-05-16 14:31:22 +0300

    Update Catalogizator.java

--- Prj02/src/com/epam/ht0/prj2/Catalogizator.java
+++ Prj02/src/com/epam/ht0/prj2/Catalogizator.java
@@ -5,7 +5,6 @@ import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-
 import java.io.IOException;
 import java.util.*;
 
